4765e09ecef80c75da94dcbda9d85fd5eb5b9f8a
Aren Clissold <arenclissold@gmail.com>
2022-06-23 16:53:30 +1000

    added resume to files and created path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        resume: resolve(__dirname, 'aren_clissold_resume.pdf')
+      }
+    }
+  },
   plugins: [react()]
 })
