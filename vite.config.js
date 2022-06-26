55ba0f7c07db4054dd9c1370aba70e6ce2cb84a3
Aren Clissold <arenclissold@gmail.com>
2022-06-27 09:43:28 +1000

    moved resume from assets to public folder and changed resume link to suit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite'
-import { resolve } from "path";
 import react from '@vitejs/plugin-react'
 
 module.exports = defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        resume: resolve(__dirname, 'src/components/assets/aren_clissold_resume.pdf')
-      },
-      output: {
-        assetFileNames: `assets/[name].[ext]`,
-      }
-    }
-  },
   plugins: [react()]
 })
