e48613a02d3443d6e64012aa579c9c71f6066177
Aren Clissold <arenclissold@gmail.com>
2025-01-17 15:53:06 +0900

    remove contact form

--- src/main.jsx
+++ src/main.jsx
@@ -6,22 +6,22 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import { Left, Right } from "./components/SideBar";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className=' text-secondary bg-[#0a1128] font-roboto overflow-hidden'>
     <Navbar />
-      <div className="grid grid-cols-10 gap-8">
-        <Left />
-        <div className="col-span-10 px-5 lg:col-span-8">
-          <Header />
-          <About />
-          <Experience />
-          <Projects />
-          <Contact />
-        </div>
-        <Right />
+    <div className="grid grid-cols-10 gap-8">
+      <Left />
+      <div className="col-span-10 px-5 lg:col-span-8">
+        <Header />
+        <About />
+        <Experience />
+        <Projects />
+        <Footer />
       </div>
+      <Right />
+    </div>
   </div>
 )
